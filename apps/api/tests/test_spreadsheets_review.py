fcaceaf728cf1e82efbf576aa863b70f8185b309
ofygel-mad <bigalmabig@gmail.com>
2026-03-13 00:10:10 +0500

    Fix auth expectation for spreadsheet preview test

--- apps/api/tests/test_spreadsheets_review.py
+++ apps/api/tests/test_spreadsheets_review.py
@@ -9,8 +9,9 @@ from apps.spreadsheets.models import SpreadsheetDocument
 
 
 @pytest.mark.django_db
-def test_spreadsheet_preview_requires_auth(api_client):
-    response = api_client.get('/api/v1/spreadsheets/00000000-0000-0000-0000-000000000000/preview/')
+def test_spreadsheet_preview_requires_auth():
+    client = APIClient()
+    response = client.get('/api/v1/spreadsheets/00000000-0000-0000-0000-000000000000/preview/')
     assert response.status_code in (401, 403)
 
 
